fd7675fc9af0640218a3a7b49dc5c27827fdf4d3
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-09-14 03:52:03 -0600

    Fix fonts

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,25 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-        {/* ========================= Google Fonts ========================= */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.gstatic.com" rel="preconnect" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400,800&display=swap"
-          rel="stylesheet"
-        />
+          {/* ========================= Google Fonts ========================= */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
+
