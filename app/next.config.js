07eb5412aa14f8291f465f71b42893915a3eacb7
Clement Balestrat <clement.balestrat@gmail.com>
2021-09-23 14:29:28 +1000

    fix

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-	async rewrites() {
-		return [{ source: '/', destination: 'https://portr.xyz' }];
-	},
 	images: {
 		loader: 'imgix',
 		path: '/',
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,8 +4,14 @@ import Header from "../sections/Header";
 import MainLayout from "../sections/MainLayout";
 import Swapper from "../sections/Swapper";
 import TransactionHistory from "../sections/TransactionHistory";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("https://portr.xyz");
+  }, []);
   return (
     <div>
       <Head>
