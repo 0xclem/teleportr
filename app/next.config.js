e7255c289b447c930832dcde463994aa91136b3a
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-05-17 02:30:32 -0400

    Redirect to Optimism bridge

--- app/next.config.js
+++ app/next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: 'https://app.optimism.io/bridge/',
+				permanent: false,
+				basePath: false,
+			},
+		];
+	},
 };
